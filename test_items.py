7052c3c0044bfb30b300135b6e76e4d6dd4d2942
DtcyfGhbikf <110355517+DtcyfGhbikf@users.noreply.github.com>
2022-08-01 23:43:21 +0300

    removed debug lines

--- test_items.py
+++ test_items.py
@@ -1,12 +1,8 @@
-# import time
 from selenium.webdriver.common.by import By
 
 
-# pytest --tb=line --language=es test_items.py
 def test_guest_should_see_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    # time.sleep(5)
     buttons = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
-
     assert len(buttons) == 1, 'Add to basket button is not visible'
